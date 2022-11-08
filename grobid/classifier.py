4b61efb616173f0eb37024af971ebfeba06ea41d
Hou Yueyuan <houyueyuan@caiyunapp.com>
2022-11-08 17:37:47 +0800

    Change args name.

--- grobid/classifier.py
+++ grobid/classifier.py
@@ -5,9 +5,9 @@ wapiti_model_map = {
     "fulltext": "/Users/hyy/mytask/b_paper/playground/Wapiti/data/fulltext_model.wapiti",
 }
 
-def classifier(model_type, features):
+def classifier(model_type, feature_path):
     if model_type not in wapiti_model_map:
         return []
 
-    results =  wapiti_infer(wapiti_model_map["model_type"], features)
+    results =  wapiti_infer(wapiti_model_map[model_type], feature_path)
     return results
