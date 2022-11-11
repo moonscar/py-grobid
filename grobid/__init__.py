4242c18746796e2d614bc78382ab63bc58eb051b
Hou Yueyuan <houyueyuan@caiyunapp.com>
2022-11-11 15:59:58 +0800

    Export alto_parser

--- grobid/__init__.py
+++ grobid/__init__.py
@@ -1,3 +1,5 @@
 from grobid.classifier import classifier
 from grobid.feature_factory import FeatureFactory
 from grobid.tei_builder import build_tei_body
+
+from grobid.cmd_utils import alto_parser
